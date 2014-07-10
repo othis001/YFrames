16be1d313c4cf280ff07bdc78b57a2691150828d
s0ph05 <jbena005@ucr.edu>
2014-07-09 20:00:21 -0700

    commit yframe.py

--- yframe.py
+++ yframe.py
@@ -1,3 +1,24 @@
 #! /usr/bin/env python
 
+from subprocess import *
+import sys
+import time
+
+#Function to convert videos to frames
+def convert(name):
+    try:
+        process = ['avconv', '-i', 'name', '-vsync', '1', '-r', '1', '-an', '-y', '/home/sophos/workspace/Y_Frames/frames%d.jpg'] 
+    except Exception, e:
+        print e
+        print 'Did not convert video'
+
+
+#Enter video url to be downloaded
+choice = raw_input('Enter url: ')
+child = Popen('youtube-dl "%s"' %choice, shell=True, stderr=PIPE)
+
+#Call function to convert video to png/jpeg frames
+convert(child)
+
+
 
