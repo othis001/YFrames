53995daa6ff40bd237e9a4d0851f284537acec0f
s0ph05 <jbena005@ucr.edu>
2014-07-09 21:11:19 -0700

    Added child.wait()

--- yframe.py
+++ yframe.py
@@ -7,7 +7,8 @@ import time
 #Function to convert videos to frames
 def convert(name):
     try:
-        process = ['avconv', '-i', 'name', '-vsync', '1', '-r', '1', '-an', '-y', '/home/sophos/workspace/Y_Frames/frames%d.jpg'] 
+        process = ['avconv', '-i', 'name', '-vsync', '1', '-r', '1', '-an', '-y', 'cwd/frames%d.jpg'] 
+        output = Popen(process, shell=True, stderr=PIPE)
     except Exception, e:
         print e
         print 'Did not convert video'
@@ -16,7 +17,7 @@ def convert(name):
 #Enter video url to be downloaded
 choice = raw_input('Enter url: ')
 child = Popen('youtube-dl "%s"' %choice, shell=True, stderr=PIPE)
-
+child.wait()
 #Call function to convert video to png/jpeg frames
 convert(child)
 
